2196b18c43b27001d6e52a3ae9915efd85936840
_uchanlee <uchanlee.dev@gmail.com>
2020-07-23 19:44:16 +0900

    💪📗[MODIFY] <src/api/auth/auth.ctrl.ts> clean code

--- src/api/auth/auth.ctrl.ts
+++ src/api/auth/auth.ctrl.ts
@@ -2,11 +2,11 @@ import { Context } from 'koa';
 import Joi from '@hapi/joi';
 import fs from 'fs';
 import { v1 as uuidv1 } from 'uuid';
-
-import loginTypeList from '../../../utils/loginTypeList';
+import loginTypeList from '../../utils/loginTypeList';
 import User from '../../models/user';
 import Post from '../../models/post';
-import { extensionList } from '../../../utils/extensionList';
+import { extensionList } from '../../utils/extensionList';
+import { deleteFile, saveFile } from '../../utils/fileControl';
 
 /*
     POST /api/auth/login
@@ -16,6 +16,7 @@ import { extensionList } from '../../../utils/extensionList';
         profileImage: 'https://google.image'
     }
 */
+
 export const login = async (ctx: Context) => {
   const schema = Joi.object().keys({
     username: Joi.string().required(),
@@ -23,34 +24,35 @@ export const login = async (ctx: Context) => {
     profileImage: Joi.string().required(),
   });
   const result = schema.validate(ctx.request.body);
+
   if (result.error) {
     ctx.status = 400;
     ctx.body = result.error;
     return;
   }
-
   const { username, email, profileImage } = ctx.request.body;
 
   try {
     let user = await User.findByEmail(email);
     if (user) {
       const token = user.generateToken();
+
       ctx.cookies.set('access_token', token, {
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true,
       });
-      return ctx.body = user;
+      ctx.body = user;
+      return;
     }
 
     const loginType = email.split('@')[1];
     if (!loginTypeList.includes(loginType)) {
-      ctx.status = 405;
+      ctx.status = 400;
       ctx.body = {
-        error: '허용되지 않는 이메일 사이트',
+        message: '허용되지 않는 이메일 사이트',
       };
       return;
     }
-
     user = new User({
       username,
       email,
@@ -58,12 +60,14 @@ export const login = async (ctx: Context) => {
       loginType,
     });
     await user.save();
-    ctx.body = user;
+
     const token = user.generateToken();
     ctx.cookies.set('access_token', token, {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true,
     });
+
+    ctx.body = user;
   } catch (e) {
     ctx.throw(500, e);
   }
@@ -83,19 +87,22 @@ export const logout = async (ctx: Context) => {
   ctx.status = 204;
 };
 
-export const leave = async (ctx: Context) => {
+export const withDraw = async (ctx: Context) => {
   const { user } = ctx.state;
   try {
     if (user) {
-      if (user.profileImage.indexOf(':') === -1) await deleteFile(user.profileImage)
+      if (user.profileImage.indexOf('http') === -1)
+        deleteFile(user.profileImage);
       const posts: any = await Post.find({ 'user.email': user.email }).exec();
+
       for (let i = 0; i < posts.length; i++) {
         for (let j = 0; j < posts[i].files.length; j++) {
-          await deleteFile(posts[i].files[j])
+          deleteFile(posts[i].files[j]);
         }
       }
-      await User.findOneAndRemove({ email: user.email });
+      await User.findOneAndRemove({ email: user.email }).exec();
       await Post.deleteMany({ 'user.email': user.email }).exec();
+
       ctx.cookies.set('access_token');
       ctx.status = 204;
       return;
@@ -109,7 +116,7 @@ export const leave = async (ctx: Context) => {
 export const update = async (ctx: any) => {
   const schema = Joi.object().keys({
     username: Joi.string().required(),
-    file: Joi.any(),
+    file: Joi.object(),
   });
   const result = schema.validate({
     ...ctx.request.body,
@@ -122,7 +129,6 @@ export const update = async (ctx: any) => {
   }
   const { username } = ctx.request.body;
   const file = ctx.request.files.file;
-  console.log(ctx.request.files)
   const fileDir = `upload/profileImage`;
   let profileData: string;
 
@@ -141,35 +147,37 @@ export const update = async (ctx: any) => {
         },
       ).exec();
       ctx.body = user;
+      return;
     } catch (e) {
       ctx.throw(500, e);
     }
   };
+
   const profileImage = ctx.state.user.profileImage;
   if (
     profileImage.includes('upload/profileImage') &&
     profileImage !== 'upload/profileImage/default.PNG'
   )
-    await deleteFile(profileImage);
+    deleteFile(profileImage);
   if (file) {
     let fileName = uuidv1();
     let extension = file.name.split('.').slice(-1)[0].toUpperCase();
     try {
       while (
-        fs.lstatSync(`public/${fileDir}/${fileName}.${extension}`).isFile()
+        fs.lstatSync(`./public/${fileDir}/${fileName}.${extension}`).isFile()
       ) {
         fileName = uuidv1();
         break;
       }
     } catch (e) {
-      console.error(e);
+      console.log(e);
     }
 
     let path = `${fileDir}/${fileName}.${extension}`;
     if (!extensionList.includes(extension)) {
-      ctx.status = 405;
+      ctx.status = 400;
       ctx.body = {
-        error: '허용되지 않은 확장자',
+        message: '허용되지 않은 확장자',
       };
       return;
     }
@@ -181,24 +189,4 @@ export const update = async (ctx: any) => {
     profileData = ctx.state.user.profileImage;
     await updateDatabase();
   }
-};
-
-const saveFile = (file: any, path: string) => {
-  return new Promise((resolve, reject) => {
-    let render = fs.createReadStream(file.path);
-    let upStream = fs.createWriteStream(`./public/${path}`);
-    render.pipe(upStream);
-    upStream.on('finish', () => {
-      resolve(path);
-    });
-    upStream.on('error', (err) => {
-      reject(err);
-    });
-  });
-};
-
-const deleteFile = (path: string) => {
-  fs.unlink(`./public/${path}`, (err) => {
-    if (err) console.error(err);
-  });
 };
