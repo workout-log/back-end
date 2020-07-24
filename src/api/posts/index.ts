c6b3e23f5ebdc5a17970f2d5fc71855914f0df56
_uchanlee <uchanlee.dev@gmail.com>
2020-07-24 11:45:56 +0900

    💪📗[MODIFY] <src/api/posts/index.ts> clean code

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -1,36 +1,36 @@
-import Router from "koa-router";
-import koaBody from "koa-body";
-import * as postCtrl from "./posts.ctrl";
+import Router from 'koa-router';
+import koaBody from 'koa-body';
+import * as postCtrl from './posts.ctrl';
 import checkLoggedIn from '../../lib/checkLoggedIn';
-import likes from "./likes";
-import comments from "./comments";
+import likes from './likes';
+import comments from './comments';
 
 const posts = new Router();
 
-posts.get("/", postCtrl.list);
+posts.get('/', postCtrl.list);
 posts.post(
-  "/",
+  '/',
   koaBody({
     multipart: true,
   }),
   checkLoggedIn,
-  postCtrl.write
+  postCtrl.write,
 );
 
 const post = new Router();
-post.get("/", postCtrl.read);
-post.delete("/", postCtrl.checkOwnPost, postCtrl.remove);
+post.get('/', postCtrl.read);
+post.delete('/', postCtrl.checkOwnPost, postCtrl.remove);
 post.patch(
-  "/",
+  '/',
   koaBody({
     multipart: true,
   }),
   postCtrl.checkOwnPost,
-  postCtrl.update
+  postCtrl.update,
 );
 
-posts.use("/:id", postCtrl.getPostById, post.routes());
-posts.use("/:id/like", checkLoggedIn, likes.routes());
-posts.use("/:id/comments", comments.routes());
+posts.use('/:id', postCtrl.getPostById, post.routes());
+posts.use('/:id/like', checkLoggedIn, likes.routes());
+posts.use('/:id/comments', comments.routes());
 
 export default posts;
