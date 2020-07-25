6fa5322343d0f9a6f5d924540a42286f26172baf
_uchanlee <uchanlee.dev@gmail.com>
2020-07-25 17:50:58 +0900

    💪📗[MODIFY] <test> connect mongo

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,7 +3,12 @@ import { expect } from 'chai';
 import { getConnectResult } from '../src';
 
 describe('Connect test!', function () {
-  it('getConnectResult should return true', () => {
-    expect(getConnectResult()).to.equal(true);
+  it('getConnectResult should return true', function (done) {
+    try {
+      expect(getConnectResult()).to.equal(false);
+      done();
+    } catch (e) {
+      done(e);
+    }
   });
 });
